fded8e6f07d5c3dbab7ccf23c7c294a9844e24c5
fionaso <41509003+fionaso@users.noreply.github.com>
2020-05-11 10:40:34 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     {
-      resolve:`gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-166054208-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-      },
-    {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
